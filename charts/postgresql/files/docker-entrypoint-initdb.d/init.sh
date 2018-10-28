c8e6077b833e6c72db922fd5a21320ce2bf91a69
Elia Videtta <evidetta@gmail.com>
2018-10-28 15:18:56 +0000

    Made change to init.sh

--- charts/postgresql/files/docker-entrypoint-initdb.d/init.sh
+++ charts/postgresql/files/docker-entrypoint-initdb.d/init.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME" <<-EOSQL
   -- Create database.
   CREATE DATABASE db_app;
 
