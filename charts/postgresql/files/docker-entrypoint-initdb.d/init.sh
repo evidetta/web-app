dbb73610cbb77269a03ef5fa588e25137ec94bfe
Elia Videtta <evidetta@gmail.com>
2018-10-28 14:57:41 +0000

    Added postgres init script

---
+++ charts/postgresql/files/docker-entrypoint-initdb.d/init.sh
@@ -0,0 +1,15 @@
+#!/#!/usr/bin/env bash
+
+set -eu
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  -- Create database.
+  CREATE DATABASE db_app;
+
+  -- Create two database users
+  CREATE ROLE readwrite LOGIN ENCRYPTED PASSWORD 'password';
+  CREATE ROLE readonly LOGIN ENCRYPTED PASSWORD 'password';
+
+  -- Give readwrite the ability to perform migrations.
+  GRANT CREATE ON SCHEMA public TO readwrite;
+EOSQL
