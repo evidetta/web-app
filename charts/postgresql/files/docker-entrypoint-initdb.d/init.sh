cd37f58712263dbcfb657c8695223fe10840032b
Elia Videtta <evidetta@gmail.com>
2018-10-28 17:32:01 +0000

    Modified init script for postgres and modified health check

--- charts/postgresql/files/docker-entrypoint-initdb.d/init.sh
+++ charts/postgresql/files/docker-entrypoint-initdb.d/init.sh
@@ -12,4 +12,25 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRESQL_USERNAME"  <<-EOSQL
 
   -- Give readwrite the ability to perform migrations.
   GRANT CREATE ON SCHEMA public TO readwrite;
+
+  ---Grant default privileges
+ALTER DEFAULT PRIVILEGES
+    FOR ROLE postgres
+    IN SCHEMA public
+    GRANT SELECT, INSERT, UPDATE ON TABLES TO readwrite;
+
+ALTER DEFAULT PRIVILEGES
+    FOR ROLE postgres
+    IN SCHEMA public
+    GRANT SELECT ON TABLES TO readonly;
+
+ALTER DEFAULT PRIVILEGES
+    FOR ROLE postgres
+    IN SCHEMA public
+    GRANT USAGE, SELECT ON SEQUENCES TO readwrite;
+
+ALTER DEFAULT PRIVILEGES
+    FOR ROLE postgres
+    IN SCHEMA public
+    GRANT SELECT ON SEQUENCES TO readonly;
 EOSQL
